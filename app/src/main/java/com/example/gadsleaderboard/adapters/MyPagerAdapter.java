1a69159d29b1125a9d9b3aa03450b66c025c6ae2
miichaelOI <52916666+miichaelOI@users.noreply.github.com>
2020-09-11 23:36:59 +0100

    third commit

---
+++ app/src/main/java/com/example/gadsleaderboard/adapters/MyPagerAdapter.java
@@ -0,0 +1,33 @@
+package com.example.gadsleaderboard.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyPagerAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+
+    public MyPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment){
+        mFragmentList.add(fragment);
+    }
+}
